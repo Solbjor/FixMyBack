669c2fd9b07309c5d07a37bc2dc27691d824ba2d
Uzair <uzairmacy@gmail.com>
2026-03-21 17:16:05 -0700

    fixed a small error

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-<<<<<<< HEAD
 // if we are deploying, this should be the deployment address
+// CHANGE THIS IP ADDRESS TO YOURS
 const BACKEND_URL = 'http://10.118.81.133:3000';
-=======
-const BACKEND_URL = 'https://backend-one-zeta-51.vercel.app/';
->>>>>>> 4df9c517addbf10acc9a815c4b204da1bdd8b350
 
 async function getToken(): Promise<string | null>{
     return await AsyncStorage.getItem('idToken');
